24dbf44017e997d0902a8106829e5de18773d7ff
Enid516 <1578802364@qq.com>
2017-05-26 23:42:36 +0800

    first commit

---
+++ app/src/main/java/com/eho/xfdemoo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.eho.xfdemoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
